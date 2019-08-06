29fd03b833babeee1fbe202e14a00e57be9a8e9c
Samflyn <akhil.sai701@gmail.com>
2019-08-06 15:41:23 +0530

    Updated Arrays

--- Arrays/src/arrayfunc/Fun.java
+++ Arrays/src/arrayfunc/Fun.java
@@ -1,33 +1,22 @@
-package insertion;
+package arrayfunc;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Insersion {
-
+public class Fun {
 	public static void main(String[] args) {
 		int [] a = new int[5];
+		int i;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter 5 numbers : ");
-		int i,j = 0,temp = 0;
 		for(i=0;i<a.length;i++)
 		{
 			a[i]=sc.nextInt();
 		}
-		for(i=0;i<a.length-1;i++)
-		{
-			for(j=i;j<a.length-1;j++)
-			{
-				if(a[i]>a[j+1])
-				{
-					temp=a[i];
-					a[i]=a[j+1];
-					a[j+1]=temp;
-				}
-			}
-		}
+		Arrays.sort(a);
 		System.out.println("After Sorting : ");
-		for(j=0;j<a.length;j++) {
-			System.out.print(a[j]+" ");
+		for(i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
 		}
 		sc.close();
 	}
