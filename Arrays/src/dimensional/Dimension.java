38cd6c7d9fc8ae2ad40a1c682d71c13cb0790a82
Samflyn <akhil.sai701@gmail.com>
2019-08-07 17:38:54 +0530

    Add OOPS

--- Arrays/src/dimensional/Dimension.java
+++ Arrays/src/dimensional/Dimension.java
@@ -3,13 +3,14 @@ package dimensional;
 import java.util.Scanner;
 
 public class Dimension {
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("how many rows");
 		int r = sc.nextInt();
 		int [][] a = new int[r][];
 		int i;
-		int j = 0;		
+		int j = 0;
 		for(i=0; i<a.length; i++)
 		{
 			System.out.println("no of columns for row "+(i+1));
@@ -18,7 +19,7 @@ public class Dimension {
 		}		
 		for(i=0;i<r;i++)
 		{
-			System.out.println("enter"+a[i].length+" elements for row "+(i+1));
+			System.out.println("enter "+a[i].length+" elements for row "+(i+1));
 			for(j=0;j<a[i].length;j++) {
 				a[i][j]=sc.nextInt();
 			}	
---
+++ OOPS/src/employee/Test.java
@@ -0,0 +1,19 @@
+package employee;
+
+public class Test {
+	public static void main(String[] args) {
+		Sample employee=new Sample();
+		
+		
+		employee.setEnpnum(1);
+		employee.setEmpname("Sammy");
+		employee.setGender('M');
+		employee.setSalary(10000);
+		employee.setStream("sailotech");
+		
+		employee.display();
+		employee.setSalary(20000);
+		employee.display();
+	}
+
+}
