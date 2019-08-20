773269645924dbf7f2ef2cf65320120a8228bfc1
Samflyn <akhil.sai701@gmail.com>
2019-08-20 16:21:05 +0530

    End of OOPS

--- OOPS/src/inheritancesingle/Child.java
+++ OOPS/src/inheritancesingle/Child.java
@@ -3,12 +3,11 @@ package inheritancesingle;
 public class Child extends Parent{
 	private int a = 30;
 	private int b = 40;
-	public void dislpay() {
+	public void display() {
 		System.out.println("Child a = "+a+"\t b = "+b);
 	}
 	public void show() {
 		super.display();
-		this.dislpay();
 		display();
 		System.out.println("Process Stop");
 	}
