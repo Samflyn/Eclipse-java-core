c1825ab3892ec6a33326e7aa0789c6122f2ddfed
Samflyn <akhil.sai701@gmail.com>
2019-09-15 17:07:55 +0530

    Update Assignment

--- InputOutputStream/src/assignments/Fifth.java
+++ InputOutputStream/src/assignments/Fifth.java
@@ -1,8 +1,10 @@
 package assignments;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -162,30 +164,58 @@ public class Fifth {
 				sum111 = sum111 + Integer.parseInt(al1111.get(i));
 			}
 			int avg111 = sum111 / al1111.size();
-			System.out.println("====================================EMP RECORDS================================");
-			System.out.println("===============================================================================");
-			System.out
-					.println("Dept \t" + "Total \t" + "Male \t" + "Female \t" + "Max \t" + "Min \t" + "Avg \t" + "CTC");
-			System.out.println("===============================================================================");
-			System.out.println(al.get(0) + "\t" + (mcounter + fcounter) + "\t" + mcounter + "\t" + fcounter + "\t"
+			String str = new String("====================================EMP RECORDS================================");
+			String str1 = new String("===============================================================================");
+			String str2 = new String(
+					"Dept \t" + "Total \t" + "Male \t" + "Female \t" + "Max \t" + "Min \t" + "Avg \t" + "CTC");
+			String str3 = new String("===============================================================================");
+			String str4 = new String(al.get(0) + "\t" + (mcounter + fcounter) + "\t" + mcounter + "\t" + fcounter + "\t"
 					+ al1.get(al1.size() - 1) + "\t" + al1.get(0) + "\t" + avg + "\t" + (mcounter + fcounter) * avg);
-			System.out.println(al.get(1) + "\t" + (mcounter1 + fcounter1) + "\t" + mcounter1 + "\t" + fcounter1 + "\t"
-					+ al11.get(al11.size() - 1) + "\t" + al11.get(0) + "\t" + avg1 + "\t"
+			String str5 = new String(al.get(1) + "\t" + (mcounter1 + fcounter1) + "\t" + mcounter1 + "\t" + fcounter1
+					+ "\t" + al11.get(al11.size() - 1) + "\t" + al11.get(0) + "\t" + avg1 + "\t"
 					+ (mcounter1 + fcounter1) * avg1);
-			System.out.println(al.get(2) + "\t" + (mcounter11 + fcounter11) + "\t" + mcounter11 + "\t" + fcounter11
-					+ "\t" + al111.get(al111.size() - 1) + "\t" + al111.get(0) + "\t" + avg11 + "\t"
+			String str6 = new String(al.get(2) + "\t" + (mcounter11 + fcounter11) + "\t" + mcounter11 + "\t"
+					+ fcounter11 + "\t" + al111.get(al111.size() - 1) + "\t" + al111.get(0) + "\t" + avg11 + "\t"
 					+ (mcounter11 + fcounter11) * avg11);
-			System.out.println(al.get(3) + "\t" + (mcounter111 + fcounter111) + "\t" + mcounter111 + "\t" + fcounter111
-					+ "\t" + al1111.get(al1111.size() - 1) + "\t" + al1111.get(0) + "\t" + avg111 + "\t"
+			String str7 = new String(al.get(3) + "\t" + (mcounter111 + fcounter111) + "\t" + mcounter111 + "\t"
+					+ fcounter111 + "\t" + al1111.get(al1111.size() - 1) + "\t" + al1111.get(0) + "\t" + avg111 + "\t"
 					+ (mcounter111 + fcounter111) * avg111);
-			System.out.println("===============================================================================");
-			System.out.println("\t" + "Total Emp : " + (mcounter + fcounter + mcounter1 + fcounter1 + mcounter11
+			String str8 = new String("===============================================================================");
+			String str9 = new String("\t" + "Total Emp : " + (mcounter + fcounter + mcounter1 + fcounter1 + mcounter11
 					+ fcounter11 + mcounter111 + fcounter111));
-			System.out.println("\t" + "Total Male : " + (mcounter + mcounter1 + mcounter11 + mcounter111));
-			System.out.println("\t" + "Total Female : " + (fcounter + fcounter1 + fcounter11 + fcounter111));
-			System.out.println("\t" + "Total CTC : " + ((mcounter + fcounter) * avg + (mcounter1 + fcounter1) * avg1
-					+ (mcounter11 + fcounter11) * avg11 + (mcounter111 + fcounter111) * avg111));
-			System.out.println("===============================================================================");
+			String str10 = new String("\t" + "Total Male : " + (mcounter + mcounter1 + mcounter11 + mcounter111));
+			String str11 = new String("\t" + "Total Female : " + (fcounter + fcounter1 + fcounter11 + fcounter111));
+			String str12 = new String(
+					"\t" + "Total CTC : " + ((mcounter + fcounter) * avg + (mcounter1 + fcounter1) * avg1
+							+ (mcounter11 + fcounter11) * avg11 + (mcounter111 + fcounter111) * avg111));
+			String str13 = new String(
+					"===============================================================================");
+			System.out.println(str + "\n" + str1 + "\n" + str2 + "\n" + str3 + "\n" + str4 + "\n" + str5 + "\n" + str6
+					+ "\n" + str7 + "\n" + str8 + "\n" + str9 + "\n" + str10 + "\n" + str11 + "\n" + str12 + "\n"
+					+ str13);
+			String path = file.getParent() + File.separator + "Result.txt";
+			File output = new File(path);
+			output.createNewFile();
+			FileOutputStream fos = null;
+			DataOutputStream dos = null;
+			try {
+				fos = new FileOutputStream(output);
+				dos = new DataOutputStream(fos);
+				dos.writeBytes(str + "\n" + str1 + "\n" + str2 + "\n" + str3 + "\n" + str4 + "\n" + str5 + "\n" + str6
+						+ "\n" + str7 + "\n" + str8 + "\n" + str9 + "\n" + str10 + "\n" + str11 + "\n" + str12 + "\n"
+						+ str13);
+				dos.flush();
+				dos.close();
+				fos.close();
+				System.out.println("Result saved to disk sucessfully.");
+			} finally {
+				if (dos != null) {
+					dos.close();
+				}
+				if (fos != null) {
+					fos.close();
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
